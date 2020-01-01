e26734176a477f1349ed725b080cc9a6fa3495c2
kiranbalampaki <kiranmgr1996@gmail.com>
2020-01-01 11:55:23 +0545

    launcher icon changed, bottom navbar's icon colors set to brand colors.

--- app/src/main/java/com/example/daraz/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/daraz/ui/account/AccountFragment.java
@@ -1,4 +1,4 @@
-package com.example.daraz.ui.notifications;
+package com.example.daraz.ui.account;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.daraz.R;
 
-public class NotificationsFragment extends Fragment {
+public class AccountFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private AccountViewModel accountViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        accountViewModel =
+                ViewModelProviders.of(this).get(AccountViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_account, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+        accountViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
