604d38396e349132278b356c2632a46426e76040
kiranbalampaki <kiranmgr1996@gmail.com>
2020-01-16 22:39:06 +0545

    Final

--- app/src/main/java/com/example/daraz/LoginRegisterActivity.java
+++ app/src/main/java/com/example/daraz/LoginRegisterActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.daraz.ui.LoginActivity;
@@ -16,6 +18,9 @@ public class LoginRegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_register);
 
         btnOpenRegisterFragment = findViewById(R.id.btnOpenRegisterFragment);
--- app/src/main/java/com/example/daraz/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/daraz/ui/home/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
